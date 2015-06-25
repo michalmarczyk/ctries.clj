4284794f5b0b624125adde0e716809a68fb85f7d
Michał Marczyk <michal.marczyk@gmail.com>
2015-06-25 18:00:47 +0200

    Depend on collection-check 0.1.6, Clojure 1.7.0-RC1

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/michalmarczyk/ctries.clj"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]]
   :jvm-opts ^:replace []
-  :profiles {:dev {:dependencies [[collection-check "0.1.4"]]}})
+  :profiles {:dev {:dependencies [[collection-check "0.1.6"]]}})
